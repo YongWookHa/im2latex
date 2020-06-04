b336a349ac72fda4080d1d72adb7ac8fc2b9b737
yongwookha <ywha12@gmail.com>
2020-06-04 09:45:14 +0900

    update: model forwarding algorithms

--- graphs/models/model.py
+++ graphs/models/model.py
@@ -2,7 +2,7 @@
 implementation of "Translating Math Formula Images to LaTeX Sequences Using
 Deep Neural Networks with Sequence-level Training"
 '''
-
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -57,7 +57,8 @@ class Im2LatexModel(nn.Module):
         self.attn_W1 = nn.Linear(dec_rnn_h, 512, bias=False)
         self.attn_W2 = nn.Linear(512, 512, bias=False)
         
-
+        self.dec_W3 = nn.Linear(dec_rnn_h*2, dec_rnn_h, bias=False)
+        self.dec_W4 = nn.Linear(dec_rnn_h, out_size, bias=False)
 
         self.attn = nn.Linear(512, 512)
         self.attn_combine = nn.Linear(512+512, 512)
@@ -66,22 +67,35 @@ class Im2LatexModel(nn.Module):
                               bidirectional=False, batch_first=True)
 
 
-    def forward(self, imgs, formulas):
+    def forward(self, imgs, formulas=None, is_train=False):
         """
         imgs: [B, C, H, W]
         formulas: [B, max_seq_len]
         returns: logit of [B, MAX_LEN, VOCAB_SIZE]
         """
+        max_seq_len = self.cfg.max_len
+
         # encoding
         memoryBank = self.encode(imgs)  # [B, 512, H', W']
+        
+        logits = []
+        bos = 0
+        logit = torch.zeros((imgs.size(0), max_seq_len))
+        prev_h, dec_states = self.init_decoder(memoryBank)
 
-        max_seq_len = formulas.size(1)
-        dec_states = self.init_decoder(memoryBank)
-
-        # LSTM input: (inp, (h_n, c_n))
-        # inp : (seq_len, batch, input_size)
-
-        logits = self.decode(dec_states, formulas, memoryBank, max_seq_len)
+        for i in range(max_seq_len):
+            if is_train:
+                tgt = formulas[:, i]
+            else:
+                tgt = torch.argmax(logit, dim=2).squeeze(1)
+            prev_w = self.embedding(tgt).unsqueeze(1)
+            C_t = self.get_attn(prev_h, memoryBank).unsqueeze(1)  # [B, 1, D]
+            h_t, dec_states =  self.LSTM(torch.cat([prev_w, prev_h], dim=2), dec_states)
+            O_t = torch.tanh(self.dec_W3(torch.cat([h_t, C_t], dim=2)))  # O_t = [B, 1, D]
+            logit = F.log_softmax(self.dec_W4(O_t), dim=2)
+            logits.append(logit)
+            
+            prev_h = h_t
 
         return torch.stack(logits).permute(1,0,2,3).squeeze(2)
 
@@ -92,23 +106,6 @@ class Im2LatexModel(nn.Module):
 
         memoryBank += add_positional_features(memoryBank)
         return memoryBank
-
-    def decode(self, dec_states, formulas, memoryBank, max_seq_len):
-        # step decoding
-        logits = []
-        prev_h, _ = dec_states  # [B, 1, D]
-        for i in range(max_seq_len):
-            tgt = formulas[:, i]
-            prev_w = self.embedding(tgt).unsqueeze(1)
-            C_t = self.get_attn(prev_h, memoryBank).unsqueeze(1)  # [B, 1, D]
-            h_t, dec_states =  self.LSTM(torch.cat([prev_w, prev_h], dim=2), dec_states)
-            O_t = torch.tanh(self.dec_W3(torch.cat([h_t, C_t], dim=2)))  # O_t = [B, 1, D]
-            logit = F.softmax(self.dec_W4(O_t), dim=2)
-            logits.append(logit)
-            
-            prev_h = h_t
-        
-        return logits    
 
     def get_attn(self, prev_h, memoryBank):
         '''
@@ -131,10 +128,9 @@ class Im2LatexModel(nn.Module):
         c1 = torch.tanh(self.init_wc1(mean_enc_out))
         h2 = torch.tanh(self.init_wh2(mean_enc_out))
         c2 = torch.tanh(self.init_wc2(mean_enc_out))
-        # o = torch.tanh(self.init_o(mean_enc_out)).unsqueeze(1)
+        o = torch.tanh(self.init_o(mean_enc_out)).unsqueeze(1)
 
-        return (torch.stack([h1, h2]), torch.stack([c1, c2]))
-        #return o, (torch.stack([h1, h2]), torch.stack([c1, c2]))
+        return o, (torch.stack([h1, h2]), torch.stack([c1, c2]))
 
 
 def add_positional_features(tensor: torch.Tensor,
