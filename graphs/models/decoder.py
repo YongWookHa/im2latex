fix: concat BOS token to the first sequence of formula (train)

graphs/models/decoder.py
@@ -31,20 +31,21 @@ class Decoder(nn.Module):
         output: probability distribution at each step [batch_size x num_steps x num_classes]
         """
         batch_size = batch_H.size(0)
-        num_steps = batch_max_length + 2  # +1 for [START], [END] tokens
+        num_steps = batch_max_length + 1  # +1 for [START], [END] tokens
 
         output_hiddens = torch.FloatTensor(batch_size, num_steps, self.hidden_size).fill_(0).to(self.device)
         hidden = (torch.FloatTensor(batch_size, self.hidden_size).fill_(0).to(self.device),
                   torch.FloatTensor(batch_size, self.hidden_size).fill_(0).to(self.device))
 
         if is_train:
+            text = torch.cat([torch.zeros(batch_size, 1).long().to(self.device), text], dim=1)
             for i in range(num_steps):
                 # one-hot vectors for a i-th char. in a batch
                 char_onehots = self._char_to_onehot(text[:, i], onehot_dim=self.num_classes)
                 # hidden : decoder's hidden s_{t-1}, batch_H : encoder's hidden H, char_onehots : one-hot(y_{t-1})
                 hidden, alpha = self.attention_cell(hidden, batch_H, char_onehots)
                 output_hiddens[:, i, :] = hidden[0]  # LSTM hidden index (0: hidden, 1: cell)
-            probs = F.softmax(self.generator(output_hiddens), dim=2)
+            probs = self.generator(output_hiddens)
 
         else:
             targets = torch.LongTensor(batch_size).fill_(0).to(self.device)  # [START] token
@@ -53,12 +54,12 @@ class Decoder(nn.Module):
             for i in range(num_steps):
                 char_onehots = self._char_to_onehot(targets, onehot_dim=self.num_classes)
                 hidden, alpha = self.attention_cell(hidden, batch_H, char_onehots)
-                probs_step = F.softmax(self.generator(hidden[0]), dim=1)
+                probs_step = self.generator(hidden[0])
                 probs[:, i, :] = probs_step
                 next_input = probs_step.argmax(1)
                 targets = next_input
 
-        return probs  # batch_size x num_steps x num_classes
+        return F.softmax(probs, dim=2)  # batch_size x num_steps x num_classes
 
 
 class AttentionCell(nn.Module):