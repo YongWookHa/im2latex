51c6dea9e4ecef9616c37ed0b931082387ba1fb2
yongwookha <ywha12@gmail.com>
2020-12-03 18:18:03 +0900

    add multiprocess

--- data/custom_preprocess_v2-1.py
+++ data/custom_preprocess_v2-1.py
@@ -1,26 +1,51 @@
 import argparse
+from multiprocessing import Pool, Manager
+from tqdm import tqdm
 from pathlib import Path
 from sympy import latex, preview
 
+options = ["-T", "tight", "-z", "0", "--truecolor", "-D 600"]
+args = None
+
+def render_latex(mlist, tup):
+    global args
+    i, formula, font = tup
+    formula = formula.strip('\n')
+    if font:
+        lat = '$$' + font + '{' + formula + '} $$'
+    else:
+        lat = '$$' + formula + '$$'
+    try:
+        preview(lat, viewer='file', filename='{}/{}.png'.format(args.output_dir, i),
+                dvioptions=options)
+        mlist.append((i, formula))
+    except Exception:
+        pass
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_txt', type=str, required=True, dest='input_txt')
+    parser.add_argument('--output_txt', type=str, required=True, dest='output_txt')
+    parser.add_argument('--output_dir', type=str, required=True, dest='output_dir')
     args = parser.parse_args()
 
-    cnt = 0
     Path('output').mkdir(exist_ok=True)
-    options = ["-T", "tight", "-z", "0", "--truecolor", "-D 600"]
-    with open(args.input_txt, 'r', encoding='utf8') as f:
-        for line in f:
-            lat = '$$'+ line.strip() + '$$'
-            preview(lat, viewer='file', filename='output/{}.png'.format(cnt), 
-                    euler=True, dvioptions=options)
-            cnt+=1
 
     with open(args.input_txt, 'r', encoding='utf8') as f:
-        for line in f:
-            lat = '$$'+ line.strip() + '$$'
-            preview(lat, viewer='file', filename='output/{}.png'.format(cnt), 
-                    euler=False, dvioptions=options)
-            cnt+=1
+        inp = f.readlines()
+    inp = [(i, formula, '') for i, formula in enumerate(inp)]      \
+        + [(i, formula, '\\mathrm') for i, formula in enumerate(inp, len(inp))] \
+        + [(i, formula, '\\mathsf') for i, formula in enumerate(inp, len(inp)*2)] \
+        + [(i, formula, '\\mathit') for i, formula in enumerate(inp, len(inp)*3)]
+    print('len:', len(inp))
+
+    p = Pool(36)
+    m = Manager()
+    mlist = m.list()
+    p.starmap(render_latex, [(mlist, x) for x in inp])
+    p.close()
+    p.join()
+
+    with open(args.output_txt, 'w', encoding='utf8') as f:
+        for i, formula in mlist:
+            f.write('{}.png\t{}\n'.format(i, formula))
